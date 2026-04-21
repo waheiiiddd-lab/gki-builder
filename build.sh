d1dc1e701a8e6208a2ee78bd7f566ac99a2e6461
zixine <waheiiiddd@gmail.com>
2026-04-21 23:23:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ elif [[ "$KVER" == "6.1" ]]; then
   KERNEL_REPO="https://github.com/linastorvaldz/kernel-android14-6.1"
   KERNEL_BRANCH="android14-6.1-lts"
 elif [[ "$KVER" == "5.10" ]]; then
-  KERNEL_REPO="https://github.com/waheiiiddd-lab/Kolor-ijo"
+  KERNEL_REPO="https://github.com/rinnsakaguchi/android_kernel_common-5.10"
   KERNEL_BRANCH="lts"
 fi
 
